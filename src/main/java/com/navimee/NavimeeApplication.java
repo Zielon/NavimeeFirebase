568ace4334e2d6c256b30e23a9b782db57597a69
Wojciech Zielonka <g36@wp.pl>
2017-10-29 14:27:01 +0100

    Scheduler

--- src/main/java/com/navimee/NavimeeApplication.java
+++ src/main/java/com/navimee/NavimeeApplication.java
@@ -1,8 +1,6 @@
 package com.navimee;
 
-import com.navimee.contracts.repositories.FacebookRepository;
 import com.navimee.contracts.repositories.NavimeeRepository;
-import com.navimee.contracts.services.FacebookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -17,15 +15,9 @@ public class NavimeeApplication extends SpringBootServletInitializer {
 
 		ConfigurableApplicationContext context = SpringApplication.run(NavimeeApplication.class, args);
 
-		FacebookService facebookService = context.getBean(FacebookService.class);
-		FacebookRepository facebookRepository = context.getBean(FacebookRepository.class);
 		NavimeeRepository navimeeRepository = context.getBean(NavimeeRepository.class);
 
 		navimeeRepository.addCities();
 		navimeeRepository.addCoordinates();
-
-		facebookRepository.addEvents(facebookService.getEvents());
-
-		System.out.println("End");
 	}
 }
---
+++ src/main/java/com/navimee/tasks/Scheduler.java
@@ -0,0 +1,23 @@
+package com.navimee.tasks;
+
+import com.navimee.contracts.repositories.FacebookRepository;
+import com.navimee.contracts.services.FacebookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+    @Autowired
+    FacebookService facebookService;
+
+    @Autowired
+    FacebookRepository facebookRepository;
+
+    @Scheduled(fixedRate = 1000 * 60 * 2)
+    public void updateEvents() {
+        facebookRepository.addEvents(facebookService.getEvents());
+        System.out.println("updateEvents");
+    }
+}
