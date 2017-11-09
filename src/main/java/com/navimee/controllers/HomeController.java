35e4728fab0cbfa3e52aebaea6bb2f81e9a14e1f
Wojciech Zielonka <g36@wp.pl>
2017-11-09 23:32:21 +0100

    resources test

--- src/main/java/com/navimee/controllers/HomeController.java
+++ src/main/java/com/navimee/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package com.navimee.controllers;
 
+import com.navimee.contracts.models.firestore.City;
+import com.navimee.contracts.models.places.Coordinate;
 import com.navimee.contracts.models.places.FacebookPlace;
 import com.navimee.contracts.repositories.palces.PlacesRepository;
+import com.navimee.mockups.NavimeeData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,9 +22,13 @@ public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public String home() {
-        return placesRepository.getPlaces("SOPOT", FacebookPlace.class)
+
+        NavimeeData navimeeData = new NavimeeData();
+        List<City> cities = navimeeData.getCities();
+
+        return cities//placesRepository.getPlaces("SOPOT", FacebookPlace.class)
                 .stream()
-                .map(c -> String.format("%s -> %s", c.name, c.id))
+                .map(c -> String.format("%s -> %s", c.name))
                 .collect(Collectors.joining("\n"));
     }
 }
