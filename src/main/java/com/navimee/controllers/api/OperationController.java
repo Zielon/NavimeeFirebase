07c15014280eda5a68e59041d27141e6da5dcfde
Wojciech Zielonka <g36@wp.pl>
2017-11-26 01:19:30 +0100

    OperationController fix

--- src/main/java/com/navimee/controllers/api/OperationController.java
+++ src/main/java/com/navimee/controllers/api/OperationController.java
@@ -1,10 +1,5 @@
 package com.navimee.controllers.api;
 
-import com.navimee.tasks.events.EventsTask;
-import com.navimee.tasks.events.SegregationTask;
-import com.navimee.tasks.places.DetailsTask;
-import com.navimee.tasks.places.PlacesTask;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/operation")
 public class OperationController {
 
-   /* @Autowired
-    PlacesTask placesTask;
-
-    @Autowired
-    EventsTask eventsTask;
-
-    @Autowired
-    SegregationTask segregationTask;
-
-    @Autowired
-    DetailsTask detailsTask;
-
     @RequestMapping(value = "update/places", method = RequestMethod.POST)
     public ResponseEntity<?> updatePlaces() {
         try{
-            placesTask.addPlacesTask();
+           // placesTask.addPlacesTask();
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -40,7 +23,7 @@ public class OperationController {
     @RequestMapping(value = "update/events", method = RequestMethod.POST)
     public ResponseEntity<?> updateFbEvents() {
         try{
-            eventsTask.addEventsTask();
+          //  eventsTask.addEventsTask();
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -50,7 +33,7 @@ public class OperationController {
     @RequestMapping(value = "update/segregate", method = RequestMethod.POST)
     public ResponseEntity<?> segregateFbEvents() {
         try{
-            segregationTask.addSegregationTask();
+          //  segregationTask.addSegregationTask();
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -60,10 +43,10 @@ public class OperationController {
     @RequestMapping(value = "update/4s/details", method = RequestMethod.POST)
     public ResponseEntity<?> fsPlacesDetails() {
         try{
-            detailsTask.addDetailsTask();
+          //  detailsTask.addDetailsTask();
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(HttpStatus.OK);
-    }*/
+    }
 }
