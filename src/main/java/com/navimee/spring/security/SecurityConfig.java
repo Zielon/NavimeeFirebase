a9de1195bd48df6862e65cc5b3273cbdaf2ab7a0
Wojciech Zielonka <g36@wp.pl>
2017-11-26 22:40:13 +0100

    removal of https

--- src/main/java/com/navimee/spring/security/SecurityConfig.java
+++ src/main/java/com/navimee/spring/security/SecurityConfig.java
@@ -17,9 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.requiresChannel().anyRequest().requiresSecure();
-
+        
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
