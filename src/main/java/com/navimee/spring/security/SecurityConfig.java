186f56379689b7dfd0a276c50b81d97792b07262
Wojciech Zielonka <g36@wp.pl>
2017-11-26 22:35:22 +0100

    https

--- src/main/java/com/navimee/spring/security/SecurityConfig.java
+++ src/main/java/com/navimee/spring/security/SecurityConfig.java
@@ -18,6 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.requiresChannel().anyRequest().requiresSecure();
+
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
