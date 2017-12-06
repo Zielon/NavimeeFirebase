92fad06e37273d8352bac4b99b9cc1dc5012f37c
Wojciech Zielonka <g36@wp.pl>
2017-12-07 00:02:21 +0100

    A bug fix. A wrong executror

--- src/main/java/com/navimee/spring/AppConfig.java
+++ src/main/java/com/navimee/spring/AppConfig.java
@@ -1,22 +1,25 @@
 package com.navimee.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableScheduling
 public class AppConfig implements SchedulingConfigurer {
 
-    @Autowired
-    ExecutorService executorService;
-
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        taskRegistrar.setScheduler(executorService);
+        taskRegistrar.setScheduler(taskScheduler());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public Executor taskScheduler() {
+        return Executors.newScheduledThreadPool(42);
     }
 }
