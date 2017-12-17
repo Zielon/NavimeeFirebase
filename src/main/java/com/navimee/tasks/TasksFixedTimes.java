20329060a1934ad10091e07bb23702bab2827a4e
Wojciech Zielonka <g36@wp.pl>
2017-12-17 11:29:19 +0100

    Fixed time changing

--- src/main/java/com/navimee/tasks/TasksFixedTimes.java
+++ src/main/java/com/navimee/tasks/TasksFixedTimes.java
@@ -3,8 +3,8 @@ package com.navimee.tasks;
 public class TasksFixedTimes {
     public static final long MINUTE = 60 * 1000;
 
-    public static final long EVENTS = MINUTE * 30;
-    public static final long HOTSPOT = MINUTE * 10;
-    public static final long NOTIFICATIONS = MINUTE * 10;
-    public static final long REMOVAL = MINUTE * 10;
+    public static final long EVENTS = MINUTE * 60;
+    public static final long HOTSPOT = MINUTE * 15;
+    public static final long NOTIFICATIONS = MINUTE * 15;
+    public static final long REMOVAL = MINUTE * 15;
 }
